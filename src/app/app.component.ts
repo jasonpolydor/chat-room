41e84eba8f4518278b1b1e2bcb54fbe2b03b17e2
jasonpolydor <jasonpolydor@gmail.com>
2018-04-22 12:23:09 +0400

    chat room

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,22 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { SigninPage } from '../pages/signin/signin';
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyDEFR0Ia9_HN_85pobNcF74mPJhGAvR0OA',
+  authDomain: 'chat-room-6a19e.firebaseapp.com',
+  databaseURL: 'https://chat-room-6a19e.firebaseio.com',
+  projectId: ' chat-room-6a19e',
+  storageBucket: 'chat-room-6a19e.appspot.com',
+};
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = SigninPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -17,6 +28,7 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+    firebase.initializeApp(config);
   }
 }
 
